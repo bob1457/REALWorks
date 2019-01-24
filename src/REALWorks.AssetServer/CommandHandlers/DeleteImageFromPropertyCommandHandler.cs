e7515c18b8204be6e67ab0c41034b732e3368ce6
bob <bob.yuan@yahoo.com>
2019-01-24 09:08:48 -0800

    Add remove images from property

---
+++ src/REALWorks.AssetServer/CommandHandlers/DeleteImageFromPropertyCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using REALWorks.AssetData;
+using REALWorks.AssetServer.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace REALWorks.AssetServer.CommandHandlers
+{
+    public class DeleteImageFromPropertyCommandHandler : IRequestHandler<DeleteImageFromPropertyCommand, bool>
+    {
+        private readonly AppDataBaseContext _context; // Inject db context for persisitence
+
+        public DeleteImageFromPropertyCommandHandler(AppDataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(DeleteImageFromPropertyCommand request, CancellationToken cancellationToken)
+        {
+            var image = _context.PropertyImg.FirstOrDefault(i => i.Id == request.Id);
+
+            _context.PropertyImg.Remove(image);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+
+                // Remove the image file from file system
+
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/REALWorks.AssetServer/Commands/DeleteImageFromPropertyCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REALWorks.AssetServer.Commands
+{
+    public class DeleteImageFromPropertyCommand: IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
